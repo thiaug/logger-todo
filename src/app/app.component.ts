1521d582cddd1fdf84beadbd79f5ad51093d3ef9
AugustoThiago <thiago.augusto.alvarenga@gmail.com>
2022-04-01 19:36:48 -0300

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, VERSION } from '@angular/core';
+import { TimerService } from './timer.service';
 
 @Component({
   selector: 'my-app',
@@ -7,4 +8,8 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  
+  constructor(public timer: TimerService){
+    this.timer.start(1000)
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TimerService } from '../timer.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public timer: TimerService) {
+    this.timer.start(1000)
+   }
 
   ngOnInit() {
   }
